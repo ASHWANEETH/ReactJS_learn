5f4e08c66c276f07ab8246403f8d49bb1e977dd6
ASHWANEETH <ashwaneeth@gmail.com>
2024-12-09 19:24:21 +0530

    props in jsx

--- src/components/NetFlixseries.jsx
+++ src/components/NetFlixseries.jsx
@@ -1,27 +1,11 @@
 import dataSeries from "../api/dataSeries.json"
-
+import { SeriesCard } from "./SeriesCard";
 export const NetFLixCard = ()=>{
  
   return(
     <ul>
-      {dataSeries.map((curEle)=> {
-        return(
-          
-          <li key={curEle.id}>
-            <div>
-              <img src={curEle.img_url} alt={curEle.name} width="20%" height="30%"/>
-            </div>
-            <h3>Name:{curEle.name}</h3>
-            <h5>Rating:{curEle.rating}</h5>
-            <p>{curEle.description}</p>
-            <p>Genere:{curEle.genre}</p>
-            <a href={curEle.watch_url}><button>Watch Now</button></a>
-            <hr />
-            </li>
-            
-          
-          );
-      })}
+      {dataSeries.map((curEle)=> <SeriesCard  key={curEle.id} curEle={curEle}/>
+      )}
       
     </ul>
   );
---
+++ src/components/SeriesCard.jsx
@@ -0,0 +1,20 @@
+
+export const SeriesCard = (props)=>{
+  console.log(props);
+  return(
+          
+    <li>
+      <div>
+        <img src={props.curEle.img_url} alt={props.curEle.name} width="20%" height="30%"/>
+      </div>
+      <h3>Name:{props.curEle.name}</h3>
+      <h5>Rating:{props.curEle.rating}</h5>
+      <p>{props.curEle.description}</p>
+      <p>Genere:{props.curEle.genre}</p>
+      <a href={props.curEle.watch_url}><button>Watch Now</button></a>
+      <hr />
+      </li>
+      
+    
+    );
+}
