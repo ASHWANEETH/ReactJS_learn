f55ccb01554f8ab02e16511654cfd1234a559dec
ASHWANEETH <ashwaneeth@gmail.com>
2024-12-12 16:53:35 +0530

    CSS module in react JS

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,4 +1,4 @@
-import "../components/SeriesCard.css";
+import styles from "../components/SeriesCard.module.css";
 export const SeriesCard = ({data,
   // greeting,children
   })=>{
@@ -11,19 +11,19 @@ export const SeriesCard = ({data,
     watch_url} = data;
 
   return(
-    <div className="card">
+    <div className={styles.card}>
 
-    <li className="card-cont">
+    <li className={styles["card-cont"]}>
  
       <div>
-        <img src={img_url} alt={name} className="cardImg"/>
+        <img src={img_url} alt={name} className={styles.cardImg}/>
       </div>
-      <div className="li-text">
+      <div className={styles["li-text"]}>
       <h3>Name:{name}</h3>
-      <h5><span className={`rating  ${rating>=8 ? "Good":"Avg"}`}>Rating:{rating}</span></h5>
+      <h5><span className={`${styles.rating}  ${rating>=8 ? styles.Good:styles.Avg}`}>Rating:{rating}</span></h5>
       <p>{description}</p>
       <p>Genere:{genre}</p>
-      <a href={watch_url} target="_blank"><button className="Watch">Watch Now</button></a>
+      <a href={watch_url} target="_blank"><button className={styles.Watch}>Watch Now</button></a>
       {/* <p>{greeting}</p>
       <p>{children}</p> */}
       </div>
@@ -32,5 +32,9 @@ export const SeriesCard = ({data,
       </div>
       
     //for Inline CSS style will get objects in {} and attributes in camelCase
+
+    //CSS---------MODULE______________________________
+    //filename.module.css import as same, className is given as js objects..
+    //for classname like "card-li" which cant be declared by '.' must be given in []
     );
 }
